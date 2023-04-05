ec7e3309f503f2b676121d1122353e9a7bb5bf1b
jidad-007 <jihadtalukder007@gmail.com>
2023-04-06 03:02:08 +0600

    do_while.c

--- Module 3/do_while.c
+++ Module 3/do_while.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
- int i;
- i=1;
-do
-{
-    printf("%d\n",i);
-    i+=1;
-}
- while(i<=10);
+     int i;
+     i=1;
+    do
+    {
+             printf("%d\n",i);
+             i=i+1;
+    }
+     while(i<=10);
  
     return 0;
 }
--- Module 3/while.c
+++ Module 3/while.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
-{
- int i;
- i=1;
- while(i<=5)
- {
-    printf("%d\n",i);
-    i=i+1;
- }
+{ 
+   int i;
+   i=1;
+   while(i<=3)
+   {  
+       printf("%d\n",i);
+        i=i+1;
+    }
 
-    return 0;
+   return 0;
+
 }
