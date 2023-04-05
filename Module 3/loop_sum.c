e56dd3b6266c5dc205c77a673b14e08b29d76813
jidad-007 <jihadtalukder007@gmail.com>
2023-04-05 17:34:58 +0600

    loop_1.c5

---
+++ Module 3/loop_sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int long long sum=0;
+    int i;
+    int n;
+    scanf("%d",&n);
+ for(i=1;i<=n;i+=1)
+ {
+    sum+=i; 
+     
+ }
+   printf("%lld",sum); 
+
+    return 0;
+}
