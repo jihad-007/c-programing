c023a5154e114ce7392dadee5a257b17011f00bc
jidad-007 <jihadtalukder007@gmail.com>
2023-04-11 05:23:59 +0600

    Q_Disits.c

---
+++ Module 6/Nested_loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+  int main()
+  {
+    int i;
+    for(int i=1;i<=10;i++)
+
+    {
+    int j;
+  for(int j=1;j<=5;j++)
+          {
+            printf("%d ",j);
+          }
+          
+          printf("\n");
+    }
+
+   
+    return 0;
+  }
---
+++ Module 6/inc_dec.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main()
+  {
+  int i=10;
+  int x=++i;
+  {
+    printf("x-%d I-%d",i,x);
+  }
+  
+    return 0;
+  }
